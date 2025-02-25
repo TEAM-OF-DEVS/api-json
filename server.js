36a1444c14d58df0a0e001906bace6c3e1a3530d
Lucas do Nascimento Queiroz <36884733+Javoso@users.noreply.github.com>
2025-02-25 01:23:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
+
+// server.use(middlewares);
+// server.use(router);
+// server.listen(3001, () => {
+//   console.log('JSON Server is running')
+// });
+
 const jsonServer = require('json-server');
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router({ posts: [], users: [] }); // Dados apenas em memória
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3001, () => {
-  console.log('JSON Server is running')
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`JSON Server rodando na porta ${PORT}`);
 });
