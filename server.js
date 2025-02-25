17592dd3866d45f5be938e35eba05c993a2f9bea
Lucas do Nascimento Queiroz <36884733+Javoso@users.noreply.github.com>
2025-02-25 01:32:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,16 +9,23 @@
 //   console.log('JSON Server is running')
 // });
 
-const jsonServer = require('json-server');
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
 
-const server = jsonServer.create();
-const router = jsonServer.router({ posts: [], users: [] }); // Dados apenas em memória
-const middlewares = jsonServer.defaults();
+const fs = require('fs')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const server = jsonServer.create()
+const router = jsonServer.router(db)
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-server.use(router);
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001
+
 server.listen(PORT, () => {
-  console.log(`JSON Server rodando na porta ${PORT}`);
-});
+  console.log(`JSON Server is running on :${PORT}`)
+})
